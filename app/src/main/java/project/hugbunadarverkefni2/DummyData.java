26f04a8298b14d93d45828329f5bcd15861e122c
sveinnvi <sveinnvi@gmail.com>
2017-03-22 22:09:39 +0000

    Fragments: List of Events and List item details

---
+++ app/src/main/java/project/hugbunadarverkefni2/DummyData.java
@@ -0,0 +1,47 @@
+package project.hugbunadarverkefni2;
+
+/**
+ * Created by svein on 22.3.2017.
+ */
+
+public class DummyData {
+
+    static String[] Headlines = {
+            "Geggjaðir tónleikar",
+            "Sundpartý",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event"
+
+    };
+
+    static String[] Articles = {
+            "Geggjaðir tónleikar\n\nGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikarGeggjaðir tónleikar",
+            "Sundpartý\n\nSundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý Sundpartý",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event",
+            "Some Event"
+    };
+}
