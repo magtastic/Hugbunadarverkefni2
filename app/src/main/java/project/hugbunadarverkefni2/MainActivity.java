54a5a188cecad4ca3cb774a288b325bb4faf33a5
sveinnvi <sveinnvi@gmail.com>
2017-04-09 17:58:13 +0000

    mainActivity final edit + delete map fragment

--- app/src/main/java/project/hugbunadarverkefni2/MainActivity.java
+++ app/src/main/java/project/hugbunadarverkefni2/MainActivity.java
@@ -1,43 +1,28 @@
 package project.hugbunadarverkefni2;
 
-import android.*;
-import android.Manifest;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
@@ -47,39 +32,15 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.vision.text.Text;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.function.Function;
 
 
-public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
     private static final int PLACE_PICKER_REQUEST = 1;
     private GoogleApiClient mGoogleApiClient;
     private ListView listOfEventsView;
-    private List<Event> events;
     private EventManager eventManager = new EventManager();
     private Location mLastLocation;
     private EditText minDayFilterInput;
@@ -93,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getPermissionToReadUserContacts();
+        getPermissionToGetLocation();
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient
@@ -107,7 +68,6 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
                     .build();
         }
 
-
         // Find the toolbar view inside the activity layout
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -117,28 +77,22 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         listOfEventsView = (ListView) findViewById(R.id.list_of_events);
     }
 
-
-
     private static final int ACCESS_LOCATION_PERMISSIONS_REQUEST = 1;
 
-    // Called when the user is performing an action which requires the app to read the
-    // user's contacts
-    public void getPermissionToReadUserContacts() {
-        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
-        // checking the build version since Context.checkSelfPermission(...) is only available
-        // in Marshmallow
-        // 2) Always check for permission (even if permission has already been granted)
+    // Called when the user is performing an action which requires the app to
+    // get location
+    public void getPermissionToGetLocation() {
+
+        // Always check for permission (even if permission has already been granted)
         // since the user can revoke permissions at any time through Settings
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
             // The permission is NOT already granted.
-            // Check if the user has been asked about this permission already and denied
-            // it. If so, we want to give more explanation about why the permission is needed.
             if ((ActivityCompat.shouldShowRequestPermissionRationale(this,
                     android.Manifest.permission.ACCESS_FINE_LOCATION))) {
-                // Show our own UI to explain to the user why we need to read the contacts
-                // before actually requesting the permission and showing the default UI
+                // TO DO :
+                // Show our own UI to explain to the user why we need to get location
             }
 
             // Fire off an async request to actually get the permission
@@ -153,11 +107,11 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String permissions[],
                                            @NonNull int[] grantResults) {
-        // Make sure it's our original READ_CONTACTS request
+        // Make sure it's our original GET_LOCATION request
         if (requestCode == ACCESS_LOCATION_PERMISSIONS_REQUEST) {
             if (grantResults.length == 1 &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Get Location permission granted", Toast.LENGTH_SHORT).show();
             } else {
                 // showRationale = false if user clicks Never Ask Again, otherwise true
                 boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale( this, android.Manifest.permission.ACCESS_FINE_LOCATION);
@@ -165,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
                 if (showRationale) {
                     // do something here to handle degraded mode
                 } else {
-                    Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Get Location permission denied", Toast.LENGTH_SHORT).show();
                 }
             }
         } else {
@@ -186,7 +140,6 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
-
             eventManager.fetchFBEvents(String.valueOf(mLastLocation.getLatitude()), String.valueOf(mLastLocation.getLongitude()), this, displayEvents);
         }
     }
@@ -205,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         final MenuItem attendeesMenuItem = menu.findItem(R.id.action_filter_attendees);
         final MenuItem daysUntilMenuItem = menu.findItem(R.id.action_filter_daysUntil);
 
+        // attendees filtering in toolbar
         MenuItemCompat.OnActionExpandListener attendeesExpandListener = new MenuItemCompat.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionExpand(MenuItem menuItem) {
@@ -259,9 +213,7 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
 
         MenuItemCompat.setOnActionExpandListener(attendeesMenuItem, attendeesExpandListener);
 
-        /////////////////////////////////////////////
-        // Days Until
-        /////////////////////////////////////////////
+        // days until filtering in toolbar
         MenuItemCompat.OnActionExpandListener daysUntilExpandListener = new MenuItemCompat.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionExpand(MenuItem menuItem) {
@@ -323,7 +275,6 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-                // Click the search icon
                 try {
                     PlacePicker.IntentBuilder intentBuilder =
                             new PlacePicker.IntentBuilder();
@@ -345,17 +296,6 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-
-        List<Event> eve = eventManager.getShownEvents();
-
-            map.addMarker(new MarkerOptions().anchor(0.35f, 0.87f).position(new LatLng(64.1265,-21.8174)).title("Titill"));
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(64.1265,-21.8174),14.0f));
-
-
-    }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
@@ -373,7 +313,7 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
         }
     }
 
-    // callback function
+    // callback function from EventManager after fetching events
     public Runnable displayEvents = new Runnable() {
         @Override
         public void run() {
@@ -394,7 +334,6 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    // Fragment
                     FragmentManager manager = getFragmentManager();
                     Fragment frag = manager.findFragmentByTag("single_event_fragment");
 
